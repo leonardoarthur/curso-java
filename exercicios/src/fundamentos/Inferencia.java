12aeadf3a8a7ca23576bebe339f8a8d853250568
Leonardo Arthur <lraguiar.la@gmail.com>
2021-09-05 16:01:06 -0300

    Fundamentos

---
+++ exercicios/src/fundamentos/Inferencia.java
@@ -0,0 +1,29 @@
+package fundamentos;
+
+public class Inferencia {
+	public static void main(String[] args) {
+		
+		//Quando o tipo da variável é difinida não podemos colocar outro tipo!!
+		double a = 5.7;
+		System.out.println(a);
+		
+		
+		var b = 8.3;
+		System.out.println(b);
+		
+		
+		var c = "texto";
+		System.out.println(c);
+		
+		
+		c = "outro texto";
+		System.out.println(c);
+				
+		
+		double d;
+		d = 157.45;
+		
+
+		
+	}
+}
